e02086f86cebbe044692be5b68723dd130b0c8a9
Lil carlitos 420 <ajedibuca@gmail.com>
2025-07-08 16:25:50 -0300

    ya
    yaanda

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
-import solidJs from "@astrojs/solid-js";
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://colegio-san-luis.vercel.app",
+  site: "https://medora-pacientes.vercel.app",
   output: "server",
   server: { host: true, port: 4321 },
   adapter: vercel(),
   integrations: [tailwind(), solidJs()],
 });
-
